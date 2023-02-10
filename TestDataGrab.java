325e626ed7c1d14bb9df523656cde2876472a0e2
Luke <112506539+9ixl3@users.noreply.github.com>
2023-02-10 12:10:49 -0600

    added grab and sort to help figure out

---
+++ TestDataGrab.java
@@ -0,0 +1,17 @@
+
+public class TestDataGrab{
+	/*String itemName;
+	Double price;
+	String location;
+
+	TestDataGrab(){
+		itemName = "Chair";
+		price = 20.52;
+		location = "Texas";
+	}
+	TestDataGrab(String Name, Double Price, String Location){
+		this.itemName = Name;
+		this.price = Price;
+		this.location = Location;
+	}*/
+}
