b0e1528ec6806d234cecc7fa1bcaa53f7fa241bc
dragon <113463234+Dragon2247@users.noreply.github.com>
2023-02-14 12:13:26 -0600

    Add files via upload

---
+++ SearchEngine.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.util.*;
+@SuppressWarnings("unused")
+public class SearchEngine{
+	static String itemType = "";
+	String material = "";
+	double price = 0.0;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	String location = "";
+	String color = "";
+	
+	
+	
+	public String location() {
+		return "";
+	}
+	
+	
+	public void dimensions() {}
+	
+	
+	public String ToString() {
+		return "";
+	}
+}
