957e0bc2ea0b6ca41022beedc3c9206472901bbd
Luke <112506539+9ixl3@users.noreply.github.com>
2023-02-22 12:35:37 -0600

    Made user input file

---
+++ UserSearchEngine.java
@@ -0,0 +1,15 @@
+
+public class UserSearchEngine{
+	
+	static String itemType = "";
+	String material = "";
+	double price = 0.0;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	String location = "";
+	String color = "";
+
+	public static void main(String[]args) {
+	}
+}
